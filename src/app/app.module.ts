eb1641523167fd1178c0229466e094dad2f03da2
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-08-08 17:13:04 -0700

    Version 1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+
+let MatApis = [
+MatToolbarModule,
+MatCardModule,
+MatButtonModule,
+MatDividerModule
+
+]
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +26,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatApis
   ],
   providers: [],
   bootstrap: [AppComponent]
